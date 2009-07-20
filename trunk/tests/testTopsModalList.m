e671ec9546395a1f8df3020e7af572ca11ad0fb6
ben <ben@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2009-07-20 22:46:46 +0000

    modal list class to support modal function loop and block helpers--eventually

---
+++ trunk/tests/testTopsModalList.m
@@ -0,0 +1,110 @@
+%% highest precedence should be at beginning of list
+clear
+list = topsModalList;
+mode = 'regularInsert';
+items = {1, 2, 3, 4, 5};
+mnemonics = {'one', 'two', 'three', 'four', 'five'};
+precedences = [-2 0 2 5 77];
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii}, precedences(ii));
+end
+gotItems = list.getSortedItemsForMode(mode);
+expectedItems = {5, 4, 3, 2, 1};
+assert(isequal(gotItems, expectedItems), 'unexpected insert order')
+
+%% insertion order should not matter
+clear
+list = topsModalList;
+mode = 'regularInsert';
+items = {5, 4, 3, 2, 1};
+mnemonics = {'five', 'four', 'three', 'two', 'one'};
+precedences = [50 40 30 20 10];
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii}, precedences(ii));
+end
+gotItems = list.getSortedItemsForMode(mode);
+expectedItems = {5, 4, 3, 2, 1};
+assert(isequal(gotItems, expectedItems), 'unexpected insert order')
+
+%% should handle nan and inf precedence
+clear
+list = topsModalList;
+mode = 'crazyInsert';
+items = {1, 2, 3, 4, 5};
+mnemonics = {'one', 'two', 'three', 'four', 'five'};
+precedences = [-inf 0 2 inf nan];
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii}, precedences(ii));
+end
+gotItems = list.getSortedItemsForMode(mode);
+expectedItems = {4, 3, 2, 1, 5};
+assert(isequal(gotItems, expectedItems), 'unexpected crazy insert order')
+
+%% should generate mnemonic structure
+clear
+list = topsModalList;
+mode = 'mnemonics';
+items = {@disp, 'thingy', 443, nan, struct};
+mnemonics = {'function', 'char', 'number', 'nan', 'struct'};
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii});
+end
+gotItems = list.getItemsByMnemonicForMode(mode);
+
+for ii = 1:length(items)
+    assert(isequalwithequalnans(gotItems.(mnemonics{ii}), items{ii}), ...
+        'unexpected mnemonic structure field')
+end
+
+%% should delete items by object or mnemonic
+clear
+list = topsModalList;
+mode = 'delete';
+items = {1, 2, 3, 4, 5};
+mnemonics = {'one', 'two', 'three', 'four', 'five'};
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii});
+end
+
+% redundant should be OK
+list.removeItemFromMode(items{1}, mode);
+list.removeItemByMnemonicFromMode(mnemonics{1}, mode);
+list.removeItemByMnemonicFromMode(mnemonics{2}, mode);
+list.removeItemFromMode(items{2}, mode);
+
+gotItems = list.getSortedItemsForMode(mode);
+expectedItems = {5, 4, 3};
+assert(isequal(gotItems, expectedItems), 'failed remove items from mode')
+
+%% should merge existing modes into new mode
+clear
+list = topsModalList;
+mode = 'mode_one';
+items = {1, 2, 3, 4, 5};
+mnemonics = {'one', 'two', 'three', 'four', 'five'};
+precedences = [-2 0 2 5 77];
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii}, precedences(ii));
+end
+
+mode = 'mode_two';
+items = {6 7 8 9 10};
+mnemonics = {'six', 'seven', 'eight', 'nine', 'ten'};
+precedences = [-2 0 2 5 77]+100;
+for ii = 1:length(items)
+    list.addItemToModeWithMnemonicWithPrecedence ...
+        (items{ii}, mode, mnemonics{ii}, precedences(ii));
+end
+
+mergedMode = 'big_mode';
+list.mergeModesIntoNewMode({'mode_one', 'mode_two'}, 'big_mode');
+
+gotItems = list.getSortedItemsForMode(mergedMode);
+expectedItems = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+assert(isequal(gotItems, expectedItems), 'failed merge modes')
