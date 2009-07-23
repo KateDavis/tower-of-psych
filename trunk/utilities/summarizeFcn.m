669e383adc1768a08fd9e3512c049662a5918ab0
ben <ben@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2009-07-23 00:54:47 +0000

    trying out property set events with a simple uitable that shows object properties and follows changes

--- trunk/utilities/summarizeFcn.m
+++ trunk/utilities/summarizeFcn.m
@@ -2,22 +2,12 @@ function functionSummary = summarizeFcn(fcn)
 %Build a readable summary of a feval()able cell array
 
 if length(fcn) > 1
-    argSummary = summarizeArgument(fcn{2});
+    argSummary = stringifyValue(fcn{2});
+    for ii = 3:length(fcn)
+        argSummary = sprintf('%s, %s', argSummary, stringifyValue(fcn{ii}));
+    end
 else
     argSummary = '';
 end
 
-for ii = 3:length(fcn)
-    argSummary = sprintf('%s, %s', argSummary, summarizeArgument(fcn{ii}));
-end
-
-functionSummary = sprintf('%s(%s)', func2str(fcn{1}), argSummary);
-
-function argSummary = summarizeArgument(arg)
-if ischar(arg)
-    argSummary = sprintf('''%s''', arg);
-elseif isnumeric(arg) && isscalar(arg)
-    argSummary = num2str(arg);
-else
-    argSummary = sprintf('%s[%s]', class(arg), num2str(size(arg)));
-end
+functionSummary = sprintf('%s(%s)', stringifyValue(fcn{1}), argSummary);
