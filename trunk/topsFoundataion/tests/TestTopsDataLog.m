0ca19ae1652bd885e651eab16a6b60bb9b237eb3
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2009-10-02 00:45:05 +0000

    writing mtest test classes for topsFoundataion

--- trunk/topsFoundataion/tests/TestTopsDataLog.m
+++ trunk/topsFoundataion/tests/TestTopsDataLog.m
@@ -1,117 +1,106 @@
-function testTopsDataLog
-%% should behave as singleton
-clear
-clc
-log1 = topsDataLog.theDataLog;
-log2 = topsDataLog.theDataLog;
-assert(isequal(log1, log2), 'log is not a singleton');
-
-
-%% should store data with mnemonics and account for them
-clear
-clc
-topsDataLog.flushAllData;
-
-% log each datum with each mnemonic
-%   redundant data would not be normal
-mnemonics = {'animals', 'pizzas', 'phone books'};
-data = {1, {'elephant', 'sauce'}, []};
-for m = mnemonics
-    for d = data
-        topsDataLog.logMnemonicWithData(m{1}, d{1});
-    end
-end
-theLog = topsDataLog.theDataLog;
-assert(theLog.count == (length(mnemonics)*length(data)), 'failed to count log entries')
-
-gotMnemnics = topsDataLog.getAllMnemonics;
-assert(isequal(sort(gotMnemnics), sort(mnemonics)), 'failed to account for unique mnemonics')
-
-
-%% should flush all data
-clear
-clc
-topsDataLog.flushAllData;
-theLog = topsDataLog.theDataLog;
-assert(theLog.count == 0, 'failed to get log with 0 entries')
-assert(isempty(topsDataLog.getAllMnemonics), 'failed to get log with no mnemonics')
-
-% log each datum with each mnemonic
-%   redundant data would not be normal
-mnemonics = {'animals', 'pizzas', 'phone books'};
-data = {1, {'elephant', 'sauce'}, []};
-for m = mnemonics
-    for d = data
-        topsDataLog.logMnemonicWithData(m{1}, d{1});
-    end
-end
-
-topsDataLog.flushAllData;
-assert(theLog.count == 0, 'failed to clear log entries after adding')
-assert(isempty(topsDataLog.getAllMnemonics), 'failed to clear log mnemonics after adding')
-
-
-
-%% should return data by mnemonic or all at once
-clear
-clc
-topsDataLog.flushAllData;
-
-% log each datum with each mnemonic
-%   redundant data would not be normal
-mnemonics = {'animals', 'pizzas', 'phone books'};
-data = {1, {'elephant', 'sauce'}, []};
-for m = mnemonics
-    for d = data
-        topsDataLog.logMnemonicWithData(m{1}, d{1});
-    end
-end
-
-for m = mnemonics
-    dataStruct = topsDataLog.getDataForMnemonic(m{1});
-    gotData = {dataStruct.data};
-    for d = data
-        count = 0;
-        for gd = gotData
-            count = count + isequal(gd, d);
-        end
-        assert(count == 1, 'failed to retrieve get exactly one of each datum')
+classdef TestTopsDataLog < TestCase
+    
+    properties
+        mnemonics;
+        data;
     end
     
-    gotMnemonics = {dataStruct.mnemonic};
-    assert(all(strcmp(gotMnemonics, m{1})), 'got wrong mnemonic')
-end
-
-
-%% should return data all at once, sorted
-clear
-clc
-topsDataLog.flushAllData;
-
-% log each datum with each mnemonic
-%   redundant data would not be normal
-mnemonics = {'animals', 'pizzas', 'phone books'};
-data = {1, {'elephant', 'sauce'}, []};
-for m = mnemonics
-    for d = data
-        topsDataLog.logMnemonicWithData(m{1}, d{1});
+    methods
+        function self = TestTopsDataLog(name)
+            self = self@TestCase(name);
+        end
+        
+        function setUp(self)
+            self.mnemonics = {'animals', 'pizzas', 'phone books'};
+            self.data = {1, {'elephant', 'sauce'}, []};
+            topsDataLog.flushAllData;
+        end
+        
+        function tearDown(self)
+            topsDataLog.flushAllData;
+        end
+        
+        function logSomeData(self)
+            % add data, redundant under each mnemonic
+            for m = self.mnemonics
+                for d = self.data
+                    topsDataLog.logMnemonicWithData(m{1}, d{1});
+                end
+            end
+        end
+        
+        function testSingleton(self)
+            log1 = topsDataLog.theDataLog;
+            log2 = topsDataLog.theDataLog;
+            assertTrue(log1==log2, 'topsDataLog should be a singleton');
+        end
+        
+        function testBasicAccounting(self)
+            self.logSomeData;
+            
+            % should get mxn log entries
+            theLog = topsDataLog.theDataLog;
+            assertEqual(theLog.count, length(self.mnemonics)*length(self.data), ...
+                'failed to count log entries');
+            
+            % should get each mnemonic, exactly once
+            loggedMnemnics = topsDataLog.getAllMnemonics;
+            assertEqual(sort(loggedMnemnics), sort(self.mnemonics), 'failed to account for unique mnemonics')
+        end
+        
+        function testDataFlush(self)
+            % log should arrive flushed, from setUp()
+            theLog = topsDataLog.theDataLog;
+            assertEqual(theLog.count, 0, 'failed to setUp log with 0 entries')
+            assertTrue(isempty(topsDataLog.getAllMnemonics), 'failed to get log with no mnemonics')
+            
+            self.logSomeData;
+            topsDataLog.flushAllData;
+            assertEqual(theLog.count, 0, 'failed to clear log entries after adding')
+            assertTrue(isempty(topsDataLog.getAllMnemonics), 'failed to clear log mnemonics after adding')
+        end
+        
+        function testDataRetrievalByMnemonic(self)
+            self.logSomeData;
+            
+            % get the same data back out
+            for m = self.mnemonics
+                dataStruct = topsDataLog.getDataForMnemonic(m{1});
+                loggedData = {dataStruct.data};
+                for d = self.data
+                    count = 0;
+                    for ld = loggedData
+                        count = count + isequal(ld, d);
+                    end
+                    assertEqual(count, 1, 'failed to retrieve exactly one of each datum');
+                end
+                
+                loggedMnemonics = {dataStruct.mnemonic};
+                assertTrue(all(strcmp(loggedMnemonics, m{1})), 'data retrieved for wrong mnemonic')
+            end
+        end
+        
+        function testDataRetrievalAllSorted(self)
+            self.logSomeData;
+            
+            % should get data, sorted by time
+            allLogged = topsDataLog.getAllDataSorted;
+            allMnemonics = {allLogged.mnemonic};
+            for m = self.mnemonics
+                assertEqual(sum(strcmp(m{1}, allMnemonics)), length(self.data), 'wrong number entries for each mnemonic')
+            end
+            
+            allData = {allLogged.data};
+            for d = self.data
+                count = 0;
+                for ld = allData
+                    count = count + isequal(ld, d);
+                end
+                assertEqual(count, length(self.mnemonics), 'wrong number entries for each datum')
+            end
+            
+            allTimes = [allLogged.time];
+            assertTrue(all(diff(allTimes) >=0), 'data log entries not sorted by time')
+        end
     end
 end
-
-allStruct = topsDataLog.getAllDataSorted;
-gotAllMnemonics = {allStruct.mnemonic};
-for m = mnemonics
-    assert(sum(strcmp(m{1}, gotAllMnemonics))==length(data), 'wrong number of mnemonics in grand data struct')
-end
-
-gotAllData = {allStruct.data};
-for d = data
-    count = 0;
-    for gd = gotAllData
-        count = count + isequal(gd, d);
-    end
-    assert(count == length(mnemonics), 'wrong number of unique data in grand data struct')
-end
-
-gotAllTimes = [allStruct.time];
-assert(all(diff(gotAllTimes) >=0), 'all data not sorted by time')
--- trunk/topsFoundataion/tests/TestTopsFunctionLoop.m
+++ trunk/topsFoundataion/tests/TestTopsFunctionLoop.m
@@ -1,84 +1,106 @@
-function testTopsFunctionLoop
+classdef TestTopsFunctionLoop < TestCase
+    
+    properties
+        functionLoop;
+        mathMode;
+        mathFunctions;
+        orderedMode;
+        orderedFunctions;
+        order;
+    end
+    
+    methods
+        function self = TestTopsFunctionLoop(name)
+            self = self@TestCase(name);
+        end
+        
+        function setUp(self)
+            self.functionLoop = topsFunctionLoop;            
 
-%% should not behave like a singleton
-clear
-clc
-loop1 = topsFunctionLoop;
-loop2 = topsFunctionLoop;
-assert(loop1~=loop2, 'failed to get unique instances')
+            self.mathMode = 'maths';
+            self.mathFunctions  = { ...
+                {@eye, 6}, ...
+                {@mod, 3, 2}};
+            
+            self.orderedMode = 'ordered';
+            self.orderedFunctions = { ...
+                {@self.executeFunction, 1}, ...
+                {@self.executeFunction, 2}, ...
+                {@self.executeFunction, 3}, ...
+                {@self.executeFunction, 4}};
+            
+            self.order = [];
+        end
+        
+        function tearDown(self)
+            delete(self.functionLoop);
+            self.functionLoop = [];
+        end
+        
+        function executeFunction(self, value)
+            self.order(end+1) = value;
+        end
+        
+        function addFunctionsToModesInOrder(self)
+            % bunch of functions for two different modes
+            for ii = 1:length(self.mathFunctions)
+                self.functionLoop.addFunctionToModeWithPrecedence(self.mathFunctions{ii}, self.mathMode, -ii);
+            end
+            
+            for ii = 1:length(self.orderedFunctions)
+                self.functionLoop.addFunctionToModeWithPrecedence(self.orderedFunctions{ii}, self.orderedMode, -ii);
+            end
+        end
+        
+        function testSingleton(self)
+            newLoop = topsFunctionLoop;
+            assertFalse(self.functionLoop==newLoop, 'topsFunctionLoop should not be a singleton');
+        end
+        
+        function testRetrieveFunctionsByModeAndPrecedence(self)
+            self.addFunctionsToModesInOrder;
+            
+            mathLoop = self.functionLoop.getFunctionListForMode(self.mathMode);
+            for ii = 1:length(mathLoop)
+                assertEqual(mathLoop{ii}, self.mathFunctions{ii}, ...
+                    'failed to add and retrieve functions for mode, in order');
+            end
 
-%% should add and preview functions
-clear
-clc
-loop = topsFunctionLoop;
-printMode = 'printouts';
-printFunctions = {{@disp, 'display'}, {@sprintf, 'sprintf %d', 3}, {@warning, 'warn'}};
-for ii = 1:length(printFunctions)
-    loop.addFunctionToModeWithPrecedence(printFunctions{ii}, printMode, -ii);
+            orderedLoop = self.functionLoop.getFunctionListForMode(self.orderedMode);
+            for ii = 1:length(orderedLoop)
+                assertEqual(orderedLoop{ii}, self.orderedFunctions{ii}, ...
+                    'failed to add and retrieve functions for mode, in order');
+            end
+        end
+
+        function testRunFunctionsByModeAndPrecedence(self)
+            self.addFunctionsToModesInOrder;
+            % run once through loop
+            self.functionLoop.runInModeForDuration(self.orderedMode, 0);
+            assertFalse(isempty(self.order), 'failed to execute functions');
+            assertTrue(all(diff(self.order))>0, 'executes functions in wrong order');
+        end
+        
+        function testPropertyChangeEventPosting(self)
+            global eventCount
+            eventCount = 0;
+            
+            % listen for event postings
+            props = properties(self.functionLoop);
+            n = length(props);
+            for ii = 1:n
+                self.functionLoop.addlistener(props{ii}, 'PostSet', @hearEvent);
+            end
+            
+            % trigger a posting for each property
+            for ii = 1:n
+                self.functionLoop.(props{ii}) = self.functionLoop.(props{ii});
+            end
+            assertEqual(eventCount, n, 'heard wrong number of property set events');
+            clear global eventCount
+            function hearEvent(metaProp, event)
+                eventCount = eventCount + 1;
+            end
+        end
+    end
 end
-
-mathMode = 'maths';
-mathFunctions = {{@eye, 77}, {@mod, 6, 3}};
-for ii = 1:length(mathFunctions)
-    loop.addFunctionToModeWithPrecedence(mathFunctions{ii}, mathMode, -ii);
-end
-
-functionLoop = loop.getFunctionListForMode(printMode);
-for ii = 1:length(functionLoop)
-    assert(isequal(functionLoop{ii}, printFunctions{ii}), ...
-        'failed to add and preview function')
-end
-
-functionLoop = loop.getFunctionListForMode(mathMode);
-for ii = 1:length(functionLoop)
-    assert(isequal(functionLoop{ii}, mathFunctions{ii}), ...
-        'failed to add and preview function')
-end
-
-%% Should run functions in order of precedence
-clear
-clc
-loop = topsFunctionLoop;
-mathMode = 'maths';
-mathFunctions = {{@eye, 77}, {@mod, 6, 3}, {@rand}};
-for ii = 1:length(mathFunctions)
-    loop.addFunctionToModeWithPrecedence(mathFunctions{ii}, mathMode, -ii);
-end
-
-% run once through loop
-loop.runInModeForDuration(mathMode, 0);
-functionList = loop.getFunctionListForMode(mathMode);
-for ii = 1:length(mathFunctions)
-    assert(isequal(functionList{ii}, mathFunctions{ii}), ...
-        'wrong function in list');
-end
-
-%% Should run dummy functions in preview mode
-clear
-clc
-loop = topsFunctionLoop;
-mathMode = 'maths';
-mathFunctions = {{@eye, 77}, {@mod, 6, 3}, {@rand}};
-for ii = 1:length(mathFunctions)
-    loop.addFunctionToModeWithPrecedence(mathFunctions{ii}, mathMode, -ii);
-end
-
-% run once through loop
-loop.previewForMode(mathMode);
-
-%% should post event when props change
-clear
-clc
-global eventCount
-eventCount = 0;
-loop = topsFunctionLoop;
-loop.addlistener('clockFcn', 'PostSet', @hearEvent);
-for ii = 1:10
-    loop.clockFcn = @now;
-end
-assert(eventCount==ii, 'heard wrong number of set events')
-clear global eventCount
-
-function hearEvent(metaProp, event)
-global eventCount
-eventCount = eventCount + 1;
