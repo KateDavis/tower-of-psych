6b06002ea00630f585e6b726b48a7ec609866006
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2009-10-02 00:45:05 +0000

    writing mtest test classes for topsFoundataion

--- topsFoundataion/tests/TestTopsBlockTree.m
+++ topsFoundataion/tests/TestTopsBlockTree.m
@@ -1,99 +1,108 @@
-function testTopsBlockTree
-
-%% should not behave like a singleton
-clear
-clc
-tree1 = topsBlockTree;
-tree2 = topsBlockTree;
-assert(tree1~=tree2, 'failed to get unique instances')
-
-%% should add and preview functions
-clear
-clc
-tree = topsBlockTree;
-topsDataLog.flushAllData;
-
-tree.name = 'test tree';
-tree.blockBeginFcn = {@disp, 'block begin'};
-tree.blockActionFcn = {@disp, 'block action'};
-tree.blockEndFcn = {@disp, 'block end'};
-
-tree.preview;
-summary = topsDataLog.getAllDataSorted;
-assert(length(summary) == 3, 'wrong number of summary functions');
-assert(isequal(tree.blockBeginFcn, summary(1).data), 'wrong block begin function');
-assert(isequal(tree.blockActionFcn, summary(2).data), 'wrong block action function');
-assert(isequal(tree.blockEndFcn, summary(3).data), 'wrong block end function');
-
-%% should add and preview children
-clear
-clc
-tree = topsBlockTree;
-topsDataLog.flushAllData;
-
-tree.name = 'test tree';
-for ii = 1:3
-    child = topsBlockTree;
-    child.name = 'child tree';
-    tree.addChild(child);
+classdef TestTopsBlockTree < TestCase
+    
+    properties
+        blockTree;
+    end
+    
+    methods
+        function self = TestTopsBlockTree(name)
+            self = self@TestCase(name);
+        end
+        
+        function setUp(self)
+            self.blockTree = topsBlockTree;
+            self.blockTree.name = 'test tree';
+            topsDataLog.flushAllData;
+        end
+        
+        function tearDown(self)
+            delete(self.blockTree);
+            self.blockTree = [];
+        end
+        
+        function testSingleton(self)
+            newBlockTree = topsBlockTree;
+            assertFalse(self.blockTree==newBlockTree, 'topsBlockTree should not be a singleton');
+        end
+        
+        function testPreviewOwnFunctions(self)
+            self.blockTree.blockBeginFcn = {@disp, 'block begin'};
+            self.blockTree.blockActionFcn = {@disp, 'block action'};
+            self.blockTree.blockEndFcn = {@disp, 'block end'};
+            
+            self.blockTree.preview;
+            summary = topsDataLog.getAllDataSorted;
+            assertEqual(length(summary), 3, 'wrong number of summary functions');
+            assertEqual(self.blockTree.blockBeginFcn, summary(1).data, 'wrong block begin function');
+            assertEqual(self.blockTree.blockActionFcn, summary(2).data, 'wrong block action function');
+            assertEqual(self.blockTree.blockEndFcn, summary(3).data, 'wrong block end function');
+        end
+        
+        function testPreviewChildFunctions(self)
+            nChildren = 3;
+            for ii = 1:nChildren
+                child = topsBlockTree;
+                child.name = 'child tree';
+                self.blockTree.addChild(child);
+            end
+            self.blockTree.preview;
+            summary = topsDataLog.getAllDataSorted;
+            assertEqual(length(summary), 3*(nChildren+1), 'wrong number of child functions logged');
+        end
+        
+        function testDepthFirstFunctionOrder(self)
+            child = topsBlockTree;
+            child.name = 'child';
+            
+            grandchild = topsBlockTree;
+            grandchild.name = 'grandchild';
+            
+            self.blockTree.addChild(child);
+            child.addChild(grandchild);
+            
+            % ordered list of functions
+            for ii = 9:-1:1
+                fcn{ii} = {@plus, ii, ii};
+            end
+            
+            % expected depth-first execution order
+            self.blockTree.blockBeginFcn = fcn{1};
+            self.blockTree.blockActionFcn = fcn{2};
+            child.blockBeginFcn = fcn{3};
+            child.blockActionFcn = fcn{4};
+            grandchild.blockBeginFcn = fcn{5};
+            grandchild.blockActionFcn = fcn{6};
+            grandchild.blockEndFcn = fcn{7};
+            child.blockEndFcn = fcn{8};
+            self.blockTree.blockEndFcn = fcn{9};
+            
+            self.blockTree.run;
+            summary = topsDataLog.getAllDataSorted;
+            for ii = 1:length(fcn)
+                assertEqual(fcn{ii}, summary(ii).data, 'functions run in wrong order');
+            end
+        end
+        
+        function testPropertyChangeEventPosting(self)
+            global eventCount
+            eventCount = 0;
+            
+            % listen for event postings
+            props = properties(self.blockTree);
+            n = length(props);
+            for ii = 1:n
+                self.blockTree.addlistener(props{ii}, 'PostSet', @hearEvent);
+            end
+            
+            % trigger a posting for each property
+            for ii = 1:n
+                self.blockTree.(props{ii}) = self.blockTree.(props{ii});
+            end
+            assertEqual(eventCount, n, 'heard wrong number of property set events');
+            clear global eventCount
+            function hearEvent(metaProp, event)
+                eventCount = eventCount + 1;
+            end
+        end
+    end
 end
-tree.preview;
-summary = topsDataLog.getAllDataSorted;
-assert(length(summary) == 3*(ii+1), 'wrong number of child blocks logged');
-
-%% should run functions and children in depth-first order
-clear
-clc
-tree = topsBlockTree;
-child = topsBlockTree;
-grandchild = topsBlockTree;
-
-topsDataLog.flushAllData;
-
-tree.name = 'test tree';
-child.name = 'child';
-grandchild.name = 'grandchild';
-tree.addChild(child);
-child.addChild(grandchild);
-
-% ordered functions
-for ii = 9:-1:1
-    fcn{ii} = {@disp, ii};
-end
-tree.blockBeginFcn = fcn{1};
-tree.blockActionFcn = fcn{2};
-child.blockBeginFcn = fcn{3};
-child.blockActionFcn = fcn{4};
-grandchild.blockBeginFcn = fcn{5};
-grandchild.blockActionFcn = fcn{6};
-grandchild.blockEndFcn = fcn{7};
-child.blockEndFcn = fcn{8};
-tree.blockEndFcn = fcn{9};
-
-tree.run;
-summary = topsDataLog.getAllDataSorted;
-for ii = 1:9
-    assert(isequal(fcn{ii}, summary(ii).data), 'function summary in wrong order');
-end
-
-
-%% should post event when props change
-clear
-clc
-global eventCount
-eventCount = 0;
-tree = topsBlockTree;
-props = properties(tree);
-n = length(props);
-for ii = 1:n
-    tree.addlistener(props{ii}, 'PostSet', @hearEvent);
-end
-for ii = 1:n
-    tree.(props{ii}) = tree.(props{ii});
-end
-assert(eventCount==length(props), 'heard wrong number of set events')
-clear global eventCount
-
-function hearEvent(metaProp, event)
-global eventCount
-eventCount = eventCount + 1;
