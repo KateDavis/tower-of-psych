ef5bbdbbcf2423cc62127a3ce43fbce9e9b27b27
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2009-11-11 18:20:36 +0000

    variables swapped in spotsTask demo, add doxygen hints file

--- demos/spots/demoSpotsTask.m
+++ demos/spots/demoSpotsTask.m
@@ -13,7 +13,7 @@ blockGUIPosition = [0 0 w h];
 logGUIPosition = [x 0 w 2*h];
 
 % configure the Spots Task
-[spotsList, spotsTree] = configureSpotsTasks(taskPosition);
+[spotsTree, spotsList] = configureSpotsTasks(taskPosition);
 
 % launch the blockTreeGUI
 blockGUI = topsBlockTreeGUI(spotsTree);
