ed627d6cfc2d4436381587c92a8fc1821b298e31
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2010-06-15 20:32:50 +0000

    starting to pull groupedListGUI functionality into portable panel class to support multiple GUIS

---
+++ foundation/guis/topsGroupedListPanel.m
@@ -0,0 +1,18 @@
+classdef topsGroupedListPanel < handle
+    % @class topsGroupedListPanel
+    % Resuable 3-column view for topsGroupedLists in topsGUI interfaces.
+    % @details
+    % Many interface classes, not just topsGroupedListGUI, need to present
+    % controls for interacting with topsGroupedList objects.
+    % topsGroupedListPanel supports topsGroupedList interactions within any
+    % topsGUI subclass.
+    % @ingroup foundataion
+    
+    properties
+        
+    end
+    
+    methods
+        
+    end
+end
