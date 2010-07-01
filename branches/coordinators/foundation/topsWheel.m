6c2c76dde9640257948b2684957975f1c9553758
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2010-07-01 21:53:54 +0000

    component is more descriptive than facet, for topsWheel

--- branches/coordinators/foundation/topsWheel.m
+++ branches/coordinators/foundation/topsWheel.m
@@ -3,60 +3,62 @@ classdef topsWheel < topsSteppable
     % Compose topsSteppable objects and run them concurrently.
     % @details
     % topsWheel objects may contain other topsSteppable objects and run
-    % them concurrently.  It uses a metaphor of a wheel with facets on it:
-    % as a wheel turns each facet may pass over the top of the wheel in
-    % sequence. Likewise, as a topsWheel object run()s, it will step() each
-    % of its facet objects in sequence.  The object will keep run()nign as
-    % long as isRunning is true for all of its facets.
+    % them concurrently.  It uses the metaphor of a turning wheel, parts of
+    % which may pass over the top over and over again, in sequence.
+    % Likewise, as a topsWheel run()s, it will step() each of its
+    % components over and over again, in sequence.  Since they will step()
+    % with the same frequency, they should appear to run all at once.  The
+    % topsWheel will stop running as soon as one of its components has
+    % isRunning equal to false.
     % @ingroup foundation
     
     properties
         % cell array of topsSteppable objects to be run() concurrently
-        facets;
+        components;
         
-        % logical array reflecting isRunning for each object in facets
-        facetIsRunning;
+        % logical array reflecting isRunning for each object in components
+        componentIsRunning;
     end
     
     methods
-        % Do a little flow control with each facets object
+        % Do a little flow control with each object in components.
         % @details
         % topsWheel extends the step() method of topsSteppable.  It calls
-        % step() on each of the topsSteppable objects in facets.
+        % step() on each of the topsSteppable objects in components.
         % @details
-        % If any of the facets objects has isRunning equal to false, this
-        % topsWheel object will set its own isRunning to false (and
-        % therefore it will stop running).
+        % If any of the components objects has isRunning equal to false,
+        % this topsWheel object will set its own isRunning to false (and
+        % therefore it should stop running).
         function step(self)
-            for ii = 1:length(self.facets)
-                self.facets{ii}.step;
-                self.facetIsRunning(ii) = self.facets{ii}.isRunning;
+            for ii = 1:length(self.components)
+                self.components{ii}.step;
+                self.componentIsRunning(ii) = self.components{ii}.isRunning;
             end
-            self.isRunning = all(self.facetIsRunning);
+            self.isRunning = all(self.componentIsRunning);
         end
         
-        % Prepare each facets object to do flow control.
+        % Prepare each object in components to do flow control.
         % @details
         % topsWheel extends the start() method of topsSteppable.  It calls
-        % start() on each of the topsSteppable objects in facets.
+        % start() on each of the topsSteppable objects in components.
         function start(self)
             self.start@topsSteppable;
-            for ii = 1:length(self.facets)
-                self.facets{ii}.start;
+            for ii = 1:length(self.components)
+                self.components{ii}.start;
             end
-            self.facetIsRunning = true(size(self.facets));
+            self.componentIsRunning = true(size(self.components));
         end
         
-        % Let each facets object finish doing flow control.
+        % Let each object in components finish doing flow control.
         % @details
         % topsWheel extends the finish() method of topsSteppable.  It calls
-        % finish() on each of the topsSteppable objects in facets.
+        % finish() on each of the topsSteppable objects in components.
         function finish(self)
             self.finish@topsSteppable;
-            for ii = 1:length(self.facets)
-                self.facets{ii}.finish;
+            for ii = 1:length(self.components)
+                self.components{ii}.finish;
             end
-            self.facetIsRunning = false(size(self.facets));
+            self.componentIsRunning = false(size(self.components));
         end
     end
 end
