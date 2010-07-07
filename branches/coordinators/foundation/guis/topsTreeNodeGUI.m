e6c5fffe92514b684284c4bbb79590f10646ea0c
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2010-07-07 01:26:55 +0000

    reimplement topsTreeNodeGUI from topsBlockTreeGUI

--- branches/coordinators/foundation/guis/topsTreeNodeGUI.m
+++ branches/coordinators/foundation/guis/topsTreeNodeGUI.m
@@ -1,28 +1,29 @@
-classdef topsBlockTreeGUI < topsGUI
-    % @class topsBlockTreeGUI
+classdef topsTreeNodeGUI < topsGUI
+    % @class topsTreeNodeGUI
     % Visualize the tree structure of an experiment.
-    % topsBlockTreeGUI shows you hiererchy of topsBlockTree "blocks" that
+    % topsTreeNodeGUI shows you hiererchy of topsTreeNode objects that
     % make up an experiment.
     % @details
     % On the left it shows you a tree browser with several buttons that
-    % represent individual blocks.  The topmost block is at the upper left
+    % represent individual nodes.  The topmost node is at the upper left
     % corner.  Its children are indented and displayed below.  Their
     % children are indented further, and so on.
     % @details
-    % You can click on an individual block to view it in detail, on the
-    % right.  The name of the block is at the top and its proerties are
-    % displayed below.  In particular, the blocks start, action, and end
-    % functions are expanded to show each function handle and arguments.
+    % You can click on an individual node to view it in detail, on the
+    % right.  The name of the node is at the top and its proerties are
+    % displayed below.  In particular, the node's start, and finish
+    % fevalables are expanded to show each function handle and its
+    % arguments.
     % @details
-    % At the top right, the "run block" button allows you to invoke the
-    % run() method of the currently displayed block.  You should probably
-    % only run the topmost block in this way.
+    % At the top right, the "run" button allows you to invoke the
+    % run() method of the currently displayed node.  You should probably
+    % only run the topmost node in this way.
     % @details
-    % You can launch topsBlockTreeGUI with the topsBlockTreeGUI()
-    % constructor, or with the gui() method a topsBlockTreeGUI.
+    % You can launch topsTreeNodeGUI with the topsTreeNodeGUI()
+    % constructor, or with the gui() method a topsTreeNodeGUI.
     % @details
-    % topsBlockTreeGUI uses listeners to detect changes to the blocks it's
-    % showing.  This means that you can view your blocks as you work on
+    % topsTreeNodeGUI uses listeners to detect changes to the nodes it's
+    % showing.  This means that you can view your nodes as you work on
     % your experiment, and you don't have to reopen or refresh the GUI.
     % @details
     % But beware that listeners can slow Matlab down.  So if you're in a
@@ -31,36 +32,37 @@ classdef topsBlockTreeGUI < topsGUI
     % @ingroup foundation
     
     properties
-        % The highest-up block to visualize in the GUI.
-        topLevelBlockTree;
+        % The highest-up node to visualize in the GUI.
+        topLevelNode;
         
-        % The block whose details are currently displayed.
-        currentBlockTree;
+        % The node whose details are currently displayed.
+        currentNode;
     end
     
     properties(Hidden)
-        blocksGrid;
-        blockDetailGrid;
-        blockRunButton;
-        blockTreeCount;
+        nodesGrid;
+        nodeDetailGrid;
+        nodeRunButton;
+        nodeCount;
     end
     
     methods
         % Constructor takes one optional argument
-        % @param topLevelBlock a block to visualize, with all its children
+        % @param topLevelNode a tree node to visualize, with all its
+        % children
         % @details
-        % Returns a handle to the new topsBlockTreeGUI.  If
-        % @a topLevelBlock is missing, the GUI will launch but no
+        % Returns a handle to the new topsTreeNodeGUI.  If
+        % @a topLevelNode is missing, the GUI will launch but no
         % data will be shown.
-        function self = topsBlockTreeGUI(topLevelBlock)
+        function self = topsTreeNodeGUI(topLevelNode)
             self = self@topsGUI;
-            self.title = 'Block Tree Viewer';
+            self.title = 'Tops Tree Viewer';
             self.createWidgets;
             
             if nargin
-                self.topLevelBlockTree = topLevelBlock;
-                self.repopulateBlocksGrid;
-                self.displayDetailsForBlock(topLevelBlock);
+                self.topLevelNode = topLevelNode;
+                self.repopulateNodesGrid;
+                self.displayDetailsForNode(topLevelNode);
             end
         end
         
@@ -69,171 +71,172 @@ classdef topsBlockTreeGUI < topsGUI
             right = 1;
             bottom = 0;
             top = 1;
-            xDiv = (1/3);
+            xDiv = .4;
             xGap = .05;
             yDiv = .95;
             
             % custom widget class, in tops/utilities
-            self.blocksGrid = ScrollingControlGrid( ...
+            self.nodesGrid = ScrollingControlGrid( ...
                 self.figure, [left, bottom, xDiv-left, yDiv-bottom]);
-            self.addScrollableChild(self.blocksGrid.panel, ...
-                {@ScrollingControlGrid.respondToSliderOrScroll, self.blocksGrid});
+            self.addScrollableChild(self.nodesGrid.panel, ...
+                {@ScrollingControlGrid.respondToSliderOrScroll, self.nodesGrid});
             
-            self.blockDetailGrid = ScrollingControlGrid( ...
+            self.nodeDetailGrid = ScrollingControlGrid( ...
                 self.figure, [xDiv+xGap, bottom, right-xDiv-xGap, yDiv-bottom]);
-            self.addScrollableChild(self.blockDetailGrid.panel, ...
-                {@ScrollingControlGrid.respondToSliderOrScroll, self.blockDetailGrid});
-            self.blockDetailGrid.rowHeight = 1.2;
+            self.addScrollableChild(self.nodeDetailGrid.panel, ...
+                {@ScrollingControlGrid.respondToSliderOrScroll, self.nodeDetailGrid});
+            self.nodeDetailGrid.rowHeight = 1.2;
             
             w = .2;
-            self.blockRunButton = uicontrol( ...
+            self.nodeRunButton = uicontrol( ...
                 'Parent', self.figure, ...
                 'BackgroundColor', self.lightColor, ...
-                'Callback', @(obj, event)self.runCurrentBlock, ...
+                'Callback', @(obj, event)self.runCurrentNode, ...
                 'Style', 'pushbutton', ...
                 'Units', 'normalized', ...
-                'String', 'run block', ...
+                'String', 'run', ...
                 'Position', [right-w, yDiv, w, top-yDiv], ...
                 'HorizontalAlignment', 'left');
         end
         
-        function displayDetailsForBlock(self, block, button)
-            self.currentBlockTree = block;
+        function displayDetailsForNode(self, node, button)
+            self.currentNode = node;
             
             if nargin > 2
-                obj = self.blocksGrid.controls;
+                obj = self.nodesGrid.controls;
                 topsText.toggleOff(obj(ishandle(obj) & obj > 0));
                 topsText.toggleOn(button);
             end
             
-            self.blockDetailGrid.deleteAllControls;
+            self.nodeDetailGrid.deleteAllControls;
             
             width = 20;
             row = 1;
-            args = self.getDescriptiveUIControlArgsForValue(block.name);
-            self.blockDetailGrid.newControlAtRowAndColumn( ...
+            args = self.getDescriptiveUIControlArgsForValue(node.name);
+            self.nodeDetailGrid.newControlAtRowAndColumn( ...
                 1, [1 width], args{:});
             
             row = row+2;
             args = self.getDescriptiveUIControlArgsForValue('iterations:');
-            self.blockDetailGrid.newControlAtRowAndColumn( ...
+            self.nodeDetailGrid.newControlAtRowAndColumn( ...
                 row, [1 4], args{:});
             
-            
-            args = self.getDescriptiveUIControlArgsForValue(block.iterations);
-            self.blockDetailGrid.newControlAtRowAndColumn( ...
+            args = self.getDescriptiveUIControlArgsForValue(node.iterations);
+            self.nodeDetailGrid.newControlAtRowAndColumn( ...
                 row, [5 6], args{:}, 'HorizontalAlignment', 'center');
             
-            args = self.getDescriptiveUIControlArgsForValue(block.iterationMethod);
-            self.blockDetailGrid.newControlAtRowAndColumn( ...
+            args = self.getDescriptiveUIControlArgsForValue(node.iterationMethod);
+            self.nodeDetailGrid.newControlAtRowAndColumn( ...
                 row, [7 width], args{:});
             
-            props = {'blockStartFevalable', 'blockActionFevalable', 'blockEndFevalable'};
-            prefixes = {block.startString, block.actionString, block.endString};
+            props = {'startFevalable', 'finishFevalable'};
+            prefixes = {node.startString, node.finishString};
             for ii = 1:length(props)
                 % label the property
                 row = row+2;
-                label =  sprintf('%s:%s', block.name, prefixes{ii});
+                label =  sprintf('%s:%s', node.name, prefixes{ii});
                 args = self.getDescriptiveUIControlArgsForValue(label);
-                self.blockDetailGrid.newControlAtRowAndColumn( ...
+                self.nodeDetailGrid.newControlAtRowAndColumn( ...
                     row, [1 width], args{:});
                 
-                fcn = block.(props{ii});
+                fcn = node.(props{ii});
                 for jj = 1:length(fcn)
                     % enumerate the function contents
                     row = row+1;
                     args = self.getInteractiveUIControlArgsForValue(fcn{jj});
-                    self.blockDetailGrid.newControlAtRowAndColumn( ...
+                    self.nodeDetailGrid.newControlAtRowAndColumn( ...
                         row, [2 width], args{:});
                 end
             end
             
-            row = row+2;
-            args = self.getDescriptiveUIControlArgsForValue('userData:');
-            self.blockDetailGrid.newControlAtRowAndColumn( ...
-                row, [1 4], args{:});
-            
-            args = self.getInteractiveUIControlArgsForValue(block.userData);
-            self.blockDetailGrid.newControlAtRowAndColumn( ...
-                row, [5 width], args{:});
-            
-            self.blockDetailGrid.repositionControls;
+            self.nodeDetailGrid.repositionControls;
         end
         
-        % Invoke the run() method of the currently displayed block.
-        % The "run block" button calls this method.  This method then calls
-        % currentBlockTree.run();
-        function runCurrentBlock(self)
-            self.currentBlockTree.run;
+        % Invoke the run() method of the currently displayed node.
+        function runCurrentNode(self)
+            self.currentNode.run;
         end
         
-        function repopulateBlocksGrid(self)
+        function repopulateNodesGrid(self)
             % delete all listeners and controls
             self.deleteListeners;
-            self.blocksGrid.deleteAllControls;
+            self.nodesGrid.deleteAllControls;
             
-            self.blockTreeCount = 1;
+            self.nodeCount = 1;
             depth = 1;
-            self.addBlockAtDepth(self.topLevelBlockTree, depth);
-            self.blocksGrid.repositionControls;
+            self.addNodeAtDepth(self.topLevelNode, depth);
+            self.nodesGrid.repositionControls;
         end
         
-        function addBlockAtDepth(self, block, depth);
-            % add this block
-            row = self.blockTreeCount;
-            col = self.getColorForString(block.name);
-            toggle = topsText.toggleText;
-            h = self.blocksGrid.newControlAtRowAndColumn( ...
-                row, [0 1]+depth, ...
-                toggle{:}, ...
-                'String', block.name, ...
-                'Callback', @(obj, event) self.displayDetailsForBlock(block, obj), ...
-                'BackgroundColor', self.lightColor, ...
-                'ForegroundColor', col);
+        function addNodeAtDepth(self, node, depth)
+            row = self.nodeCount;
+            col = self.getColorForString(node.name);
             
-            % listen to this block
-            self.listenToBlockTree(block);
-            
-            % recur on children
-            for ii = 1:length(block.children)
-                self.blockTreeCount = self.blockTreeCount + 1;
-                self.addBlockAtDepth(block.children(ii), depth+1);
+            if isa(node, 'topsTreeNode')
+                % add this node
+                toggle = topsText.toggleText;
+                h = self.nodesGrid.newControlAtRowAndColumn( ...
+                    row, [0 1]+depth, ...
+                    toggle{:}, ...
+                    'String', node.name, ...
+                    'Callback', @(obj, event) self.displayDetailsForNode(node, obj), ...
+                    'BackgroundColor', self.lightColor, ...
+                    'ForegroundColor', col);
+                
+                % listen to this node
+                self.listenToNode(node);
+                
+                % recur on children
+                children = node.children.allItems;
+                for ii = 1:length(children)
+                    self.nodeCount = self.nodeCount + 1;
+                    self.addNodeAtDepth(children{ii}, depth+1);
+                end
+                
+            else
+                static = topsText.staticText;
+                h = self.nodesGrid.newControlAtRowAndColumn( ...
+                    row, [0 1]+depth, ...
+                    static{:}, ...
+                    'String', node.name, ...
+                    'BackgroundColor', self.lightColor, ...
+                    'ForegroundColor', col);
             end
         end
         
-        function listenToBlockTree(self, block)
-            props = properties(block);
-            for ii = 1:length(props)
-                listener = block.addlistener(props{ii}, 'PostSet', ...
-                    @(source, event)self.hearBlockPropertyChange(source, event));
-                self.addListenerWithName(listener, props{ii});
-            end
+        function listenToNode(self, node)
+            % props = properties(node);
+            % for ii = 1:length(props)
+            %     listener = node.addlistener(props{ii}, 'PostSet', ...
+            %         @(source, event)self.hearNodePropertyChange(source, event));
+            %     self.addListenerWithName(listener, props{ii});
+            % end
             
-            listener = block.addlistener('BlockStart', ...
-                @(source, event)self.hearBlockStart(source, event));
-            self.addListenerWithName(listener, 'BlockStart');
+            listener = node.addlistener('RunStart', ...
+                @(source, event)self.hearRunStart(source, event));
+            self.addListenerWithName(listener, 'RunStart');
         end
         
-        function hearBlockPropertyChange(self, metaProp, event)
+        function hearNodePropertyChange(self, metaProp, event)
             % rebuild when tree structure or name changes
             if any(strcmp(metaProp.Name, {'children', 'parent', 'name'}))
-                self.repopulateBlocksGrid;
+                self.repopulateNodesGrid;
             end
             
-            % redraw for currently detailed block
-            if event.AffectedObject == self.currentBlockTree
-                self.displayDetailsForBlock(self.currentBlockTree);
+            % redraw for currently detailed node
+            if event.AffectedObject == self.currentNode
+                self.displayDetailsForNode(self.currentNode);
             end
         end
         
-        function hearBlockStart(self, block, event)
-            self.displayDetailsForBlock(block);
+        function hearRunStart(self, node, event)
+            self.displayDetailsForNode(node);
         end
         
         function repondToResize(self, figure, event)
             % attempt to resize with characters, rather than normalized
-            self.blocksGrid.repositionControls;
-            self.blockDetailGrid.repositionControls;
+            self.nodesGrid.repositionControls;
+            self.nodeDetailGrid.repositionControls;
         end
     end
 end
