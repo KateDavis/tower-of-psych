4fac97a2402bce5ec56c8934d6850d591729e3c1
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2010-10-27 01:48:11 +0000

    updating tops demo comments and deleting obsolete text file

--- demos/demoTopsText.m
+++ demos/demoTopsText.m
@@ -1,7 +1,11 @@
+% Demonstrate some flavors of the text widgets that Tower of Psych uses.
 close all
 clear
 clear classes
 
+% First, define some attributes for use in this demo.  Since topsText is
+% based on the Matlab built-in uicontrol class, these must be valid
+% uicontrol property names and values.
 lookFeel = { ...
     'Units', 'normalized', ...
     'BackgroundColor', [1 1 1], ...
@@ -9,6 +13,9 @@ lookFeel = { ...
     'FontWeight', 'bold', ...
     'Callback', @(obj, event)disp(get(obj, 'String'))};
 
+% Use the topsText class to get additional attributes for a topsText-style
+% static text uicontrol object.  Combine the arguments and create a
+% topsText-style uicontrol object in a new figure.
 args = topsText.staticText;
 more = { ...
     'String', 'static', ...
@@ -16,6 +23,8 @@ more = { ...
     };
 static = uicontrol(args{:}, lookFeel{:}, more{:});
 
+% Similarly, create a topsText-style clickable text uicontrol object--like
+% a button, but with a special topsText appearance.
 args = topsText.clickText;
 more = { ...
     'String', 'click', ...
@@ -23,6 +32,8 @@ more = { ...
     };
 click = uicontrol(args{:}, lookFeel{:}, more{:});
 
+% Create a topsText-style togglable text uicontrol object--like
+% a toggle button, but with a special topsText appearance.
 args = topsText.toggleText;
 more = { ...
     'String', 'toggle', ...
@@ -30,6 +41,11 @@ more = { ...
     };
 toggle = uicontrol(args{:}, lookFeel{:}, more{:});
 
+% Create a topsText-style editable text uicontrol object--like
+% a text edit field, but with a special topsText appearance.
+%
+% This style of editable text will only allow the user to edit the text
+% value that appears in the widget.
 args = topsText.editText;
 more = { ...
     'String', 'edit', ...
@@ -37,17 +53,32 @@ more = { ...
     };
 editable = uicontrol(args{:}, lookFeel{:}, more{:});
 
+% Even better, create a topsText-style editable text uicontrol object which
+% allows value that appears in the widget to be bound to a property of
+% another object!
+
+% create an object to work with and set a value, nested deep in its
+% userData property.  Also define the "subs" path that digs into the nest.
 object = EventWithData;
 object.userData.a{4} = 'get and set';
 subs = substruct('.', 'userData', '.', 'a', '{}', {4});
 
+% define set and get functions that will bind the object property to the
+% text widget.  The getter passes values from the object property to the
+% text widget.  The setter passes user input values from the text widget to
+% the object property.
 getter = {@subsref, object, subs};
 setFunction = @(value, object, subs)subsasgn(object, subs, value);
 setter = {setFunction, object, subs};
 
+% create the object-bound text widget with the getter and setter
 args = topsText.editTextWithGetterAndSetter(getter, setter);
 more = { ...
     'Position', subposition([0 0 1 1], 10, 3, 9, 2), ...
     'Callback', @(obj, event)disp(feval(getter{:})), ...
     };
 bound = uicontrol(args{:}, lookFeel{:}, more{:});
+
+% note that editing the value in this bound widget will alter the value of
+% object.userData.  Check it out on the command line!
+disp(object.userData)
