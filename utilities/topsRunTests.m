ac6b3b7dc0ebd56f9636cc3740d9d7ba4e724c4f
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2011-08-25 22:32:50 +0000

    topsRunTests replaces runAllTopsTests, for cleaner, more flexible testing.  fixed colorspec warning from some gui components

---
+++ utilities/topsRunTests.m
@@ -0,0 +1,64 @@
+%Invoke Tower of Psych unit tests at or below the current folder.
+% @param fileFilter optional regular expression to filter test files
+% @details
+% Runs unit tests for Tower of Psych.  Searches the current folder and
+% subfolders for files whose names begin or entd with "test" or "Test", and
+% executes any matlab u-unit tests they define. If @a fileFilter is
+% supplied, limits tests to those files that match @a fileFilter.
+% @details
+% Attempts to avoid sequential effects by invoking multiple "clear"
+% statements between test files.
+% @details
+% If all unit tests are successful, returns true.  If any unit test fails,
+% aborts and returns false.
+% @details
+% Here are some examples:
+% everythingPassed = topsRunTests();
+% foundationPassed = topsRunTests('foundation');
+% @ingroup utilities
+function didPass = topsRunTests(fileFilter)
+tic();
+close all
+evalin('base', 'clear all global');
+evalin('base', 'clear classes');
+evalin('base', 'clear mex');
+
+if nargin < 1
+    fileFilter = '';
+end
+
+initialFolder = pwd();
+fileList = findFiles(initialFolder, fileFilter);
+nFiles = numel(fileList);
+
+disp(sprintf('\nRUNNING UNIT TESTS FROM %d FILES\n', nFiles));
+
+didPass = true;
+for ii = 1:nFiles
+    close all
+    evalin('base', 'clear all global');
+    evalin('base', 'clear mex');
+    drawnow();
+    
+    [filePath, fileName] = fileparts(fileList{ii});
+    if ~isempty(regexpi(fileName, '^test')) ...
+            || ~isempty(regexpi(fileName, 'test$'))
+        cd(filePath);
+        suite = TestSuite.fromName(fileName);
+        if ~isempty(suite.TestComponents)
+            disp(sprintf('\nRUNNING UNIT TESTS FROM FILE %d/%d:\n%s', ...
+                ii, nFiles, fileList{ii}));
+            didPass = suite.run();
+        end
+    end
+    
+    if ~didPass
+        break;
+    end
+end
+
+cd(initialFolder);
+
+if didPass
+    disp(sprintf('\nPASSED ALL UNIT TESTS IN %f SECONDS\n', toc()));
+end
