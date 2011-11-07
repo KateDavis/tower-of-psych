e385f5a00dad286a919b8ae41f9570a82a5c89b8
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2011-11-07 14:59:48 +0000

    topsCallList.addCall()  can replace a call with the same name

--- trunk/foundation/topsCallList.m
+++ trunk/foundation/topsCallList.m
@@ -4,9 +4,9 @@ classdef topsCallList < topsConcurrent
     % topsCallList manages a list of functions to be called as a batch.
     % @details
     % Since topsCallList extends topsConcurrent, a topsCallList object can
-    % be added as one of the components of a topsConcurrentComposite object, and its
-    % batch of functions can be invoked concurrently with other topsConcurrent
-    % objects.
+    % be added as one of the components of a topsConcurrentComposite
+    % object, and its batch of functions can be invoked concurrently with
+    % other topsConcurrent objects.
     % @details
     % A topsCallList object has no internal state to keep track of, so it
     % has no natural way to decide when it's done running.  The
@@ -24,8 +24,10 @@ classdef topsCallList < topsConcurrent
     % feval(foo{:}).
     % @details
     % By default, all of the fevalables in the call list will be called
-    % during each runBriefly().  Optionally, each call can be given a name and
-    % its runBriefly() activity can be toggled with setActiveByName().
+    % during each runBriefly().  Optionally, each call can be given a name
+    % and its runBriefly() activity can be toggled with setActiveByName().
+    % Or, one call can replaced with a different call, useing
+    % setCallByName();
     % @ingroup foundation
     
     properties (SetObservable)
@@ -43,28 +45,52 @@ classdef topsCallList < topsConcurrent
     
     methods
         % Add an "fevalable" to the call list.
-        % @param fevalable a cell array whose contents to pass to feval()
+        % @param fevalable a cell array with contents to pass to feval()
         % @param name optional string name to assign to @a fevalable
         % @details
-        % Appends the given @a fevalable to the calls struct array.
-        % If @a name is provided, @a fevalable can be referred to by @a
-        % name.  If no @a name is provided, uses a generic name.
+        % Appends or inserts the given @a fevalable to the calls struct
+        % array.  If @a name is provided, @a fevalable can be referred to
+        % later by @a name.  It will replace any existing fevalable with
+        % the same name.  If @a name is omitted, addCall() assigns a
+        % generic name and appends @a fevalable to the end of the calls
+        % array.
         % @details
-        % Returns the index into the calls string array where @a fevalable
-        % was appended.
-        function n = addCall(self, fevalable, name)
+        % Returns the index into the calls struct array where @a fevalable
+        % was appended or inserted.
+        function index = addCall(self, fevalable, name)
             if nargin < 3
                 name = self.defaultName;
             end
+            
             newCall = struct( ...
                 'fevalable', {fevalable}, ...
                 'name', name, ...
                 'isActive', true);
-
+            
             if isempty(self.calls)
+                % initialize the calls struct array
+                index = 1;
                 self.calls = newCall;
+                
             else
-                self.calls(end+1) = newCall;
+                if strcmp(name, self.defaultName);
+                    % append with the default name
+                    index = numel(self.calls) + 1;
+                    
+                else
+                    names = {self.calls.name};
+                    isReplacement = strcmp(name, names);
+                    if any(isReplacement)
+                        % replace by matching name
+                        index = find(isReplacement, 1, 'first');
+                        
+                    else
+                        % append with the new name
+                        index = numel(self.calls) + 1;
+                    end
+                end
+                
+                self.calls(index) = newCall;
             end
         end
         
@@ -74,8 +100,9 @@ classdef topsCallList < topsConcurrent
         % fevalable during runBriefly()
         % @details
         % Determines whether the named fevalable function call in the calls
-        % struct array will be invoked during runBriefly().  If multiple calls
-        % have the same name, @a isActive will be applied to all of them.
+        % struct array will be invoked during runBriefly().  If multiple
+        % calls have the same name, @a isActive will be applied to all of
+        % them.
         function setActiveByName(self, isActive, name)
             named = strcmp({self.calls.name}, name);
             [self.calls(named).isActive] = deal(isActive);
@@ -83,12 +110,14 @@ classdef topsCallList < topsConcurrent
         
         % Invoke active calls in a batch.
         function runBriefly(self)
-            isActive = [self.calls.isActive];
-            fevalables = {self.calls(isActive).fevalable};
-            for ii = 1:length(fevalables)
-                feval(fevalables{ii}{:});
+            if ~isempty(self.calls)
+                isActive = [self.calls.isActive];
+                fevalables = {self.calls(isActive).fevalable};
+                for ii = 1:length(fevalables)
+                    feval(fevalables{ii}{:});
+                end
+                self.isRunning = self.isRunning && self.alwaysRunning;
             end
-            self.isRunning = self.isRunning && self.alwaysRunning;
         end
     end
 end
