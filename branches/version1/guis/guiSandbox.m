4f83d1bb2b542a4a8e793f11f065cbe9a944094c
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2012-02-09 23:16:38 +0000

    new utility to make a drill-down gui for any item

--- branches/version1/guis/guiSandbox.m
+++ branches/version1/guis/guiSandbox.m
@@ -1,29 +1,6 @@
-%%
-asdf;
-
-fig = topsFigure('george');
-
-bl = topsPanel(fig);
-set(bl.pan, 'BackgroundColor', fig.colors(1,:));
-
-br = topsPanel(fig);
-set(br.pan, 'BackgroundColor', fig.colors(2,:));
-
-t = topsPanel(fig);
-set(t.pan, 'BackgroundColor', fig.colors(3,:));
-
-%fig.setPanels({bl br;t t}, [3 7], [4 6])
-fig.setPanels({bl br})
-
-%%
-asdf;
-
-fig = topsFigure('smappy');
-infoPan = topsInfoPanel(fig);
-drillDownPan = topsDrillDownPanel(fig);
-fig.setPanels({drillDownPan infoPan});
-
-name = 'thisItem';
+%% Put the drill-down GUI through its paces
+close all
+clear all
 
 items = { ...
     sprintf('Cheese this man...\n...and quickly!'), ...
@@ -37,16 +14,36 @@ items = { ...
     false(1,100), ...
     eye(9)};
 
-for ii = 1:numel(items)
-    drillDownPan.setBaseItem(items{ii}, name);
-    fig.setCurrentItem(items{ii}, name);
-    pause();
+nItems = numel(items);
+for ii = 1:nItems
+    name = sprintf('item %d/%d', ii, nItems);
+    fig = topsMakeDrillDownGUI(items{ii}, name);
+    while ishandle(fig.fig);
+        drawnow();
+    end
 end
 
+%% Make sure the figure can do panel layout
 close all
+clear all
 
-%%
-asdf;
+fig = topsFigure('george');
+
+bl = topsPanel(fig);
+set(bl.pan, 'BackgroundColor', fig.colors(1,:));
+
+br = topsPanel(fig);
+set(br.pan, 'BackgroundColor', fig.colors(2,:));
+
+t = topsPanel(fig);
+set(t.pan, 'BackgroundColor', fig.colors(3,:));
+
+fig.setPanels({bl br;t t}, [3 7], [4 6])
+
+%% Look at a uitable
+close all
+clear all
+
 fig = topsFigure('hoo');
 table = fig.makeUITable();
 set(table, ...
---
+++ branches/version1/guis/topsMakeDrillDownGUI.m
@@ -0,0 +1,26 @@
+% GUI for "drilling down" into elements, fields, and properties of an item.
+% @param item any item
+% @param itemName string name to display for @a item
+% @details
+% Opens a new GUI figure for summarizing the given @a item and "driling
+% down" to explore any elements, fields, and properties, to arbitrary
+% depth.  If @a itemName is provided, displays @a itemName to represent @a
+% item.
+function fig = topsMakeDrillDownGUI(item, itemName)
+
+if nargin < 2
+    itemName = 'item';
+end
+
+% make a top-level figure with standard appearance and buttons
+fig = topsFigure(sprintf('drill down for %s', itemName));
+
+% make a panel to explore various sub-items
+drillDownPan = topsDrillDownPanel(fig);
+drillDownPan.setBaseItem(item, itemName);
+
+% make a panel to describe each selected sub-item
+infoPan = topsInfoPanel(fig);
+
+% add the panels to the GUI figure
+fig.setPanels({drillDownPan, infoPan});
