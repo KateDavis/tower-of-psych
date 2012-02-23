f49a08190e452d23f7f4547ddab7cd48ed39269a
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2012-02-23 21:14:55 +0000

    done with branch for GUI redesign

--- branches/redesignGUIS/utilities/TowerOfPsych.m
+++
@@ -1,2 +0,0 @@
-function TowerOfPsych
-%A placeholder to indicate that Tower of Psych is on the Matlab path
--- branches/redesignGUIS/utilities/topsRoot.m
+++
@@ -1,4 +0,0 @@
-% Get the absolute path to the Tower of Psych project root.
-function topsPath = topsRoot()
-utilitiesPath = fileparts(which('TowerOfPsych'));
-topsPath = fullfile(utilitiesPath, '..');
