6e6c26ef769ba3136f3d5f4227680bb234667e87
Benjamin.Heasly <Benjamin.Heasly@e98bbcd8-aeb6-11de-b3ee-c79121bffc40>
2012-03-14 20:35:15 +0000

    remaking hits for doxygen HTML main page

--- utilities/doxygen.m
+++ utilities/doxygen.m
@@ -3,3 +3,16 @@
 % @defgroup utilities
 % @defgroup demos
 %
+% @mainpage Tower of Psych Reference
+% @par
+% Classes:
+%   - <a href="classes.html">Class Index</a>
+%   - <a href="annotated.html">Class Descriptions</a>
+%   - <a href="inherits.html">Class Heirarchy</a>
+%   .
+% @par
+% Functions:
+%   - <a href="group__utilities.html">Utilities</a>
+%   - <a href="group__demos.html">Demos</a>
+%   .
+%
